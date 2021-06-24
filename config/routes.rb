17559b0f5984787768d8dc8d46337a41b94430e8
Francisco <fnaveira11@gmail.com>
2021-06-24 14:12:50 -0400

    Done.

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,15 @@
 Rails.application.routes.draw do
+  resources :users, only: [:show] do
+    resources :items, only: [:index, :show, :create]
+  end
+
   resources :items, only: [:index]
-  resources :users, only: [:show]
 end
+
+# Prefix Verb URI Pattern                            Controller#Action
+
+# user_items GET  /users/:user_id/items(.:format)     items#index
+#           POST  /users/:user_id/items(.:format)     items#create
+#  user_item GET  /users/:user_id/items/:id(.:format) items#show
+#       user GET  /users/:id(.:format)                users#show
+#      items GET  /items(.:format)                    items#index
