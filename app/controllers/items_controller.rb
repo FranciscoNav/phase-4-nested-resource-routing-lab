8078a6160f4f23a229c1c272f922d7da14fb0c58
Francisco <fnaveira11@gmail.com>
2021-06-24 15:41:18 -0400

    commit tree ed8b542c96e055a3355bc01f4f88900fad63b9c9

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -2,8 +2,9 @@ class ItemsController < ApplicationController
   rescue_from ActiveRecord::RecordNotFound, with: :render_not_found_response
 
   def index
-    if params[:user_id]
-      user = User.find_by(id: params[:user_id])
+
+    if item_params[:user_id]
+      user = User.find(item_params[:user_id])
       items = user.items
     else
       items = Item.all
@@ -12,25 +13,26 @@ class ItemsController < ApplicationController
   end
 
   def show
-    user = User.find_by(id: params[:user_id])
-    item = user.items.find_by(id: params[:id])
+    user = User.find(item_params[:user_id])
+    item = user.items.find(params[:id])
     render json: item
   end
 
   def create
-    user = User.find_by(id: params[:user_id])
+    user = User.find(item_params[:user_id])
     item = user.items.create(item_params)
     render json: item, status: :created
   end
 
   private
 
-  def render_not_found_response
-    render json: { error: "User not found" }, status: :not_found
+  def render_not_found_response(exception)
+    render json: { error: "#{exception.model} not found" }, status: :not_found
   end
 
+
   def item_params
-    params.permit(:name, :description, :price)
+    params.permit(:name, :description, :price, :user_id)
   end
 
 end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,5 @@
 class UsersController < ApplicationController
-  rescue_from ActiveRecord::RecordNotFound, with: :render_not_found_response
+  # rescue_from ActiveRecord::RecordNotFound, with: :render_not_found_response
 
   def show
     user = User.find_by(id: params[:id])
@@ -8,8 +8,8 @@ class UsersController < ApplicationController
 
   private
   
-  def render_not_found_response
-    render json: { error: "User not found" }, status: :not_found
-  end
+  # def render_not_found_response
+  #   render json: { error: "User not found" }, status: :not_found
+  # end
 
 end
